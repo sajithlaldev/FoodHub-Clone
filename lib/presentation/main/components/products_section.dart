48758d0e4f686fc324fca56c251c712ea4a1b887
Sajithlal <sajithlal.mk@fjordstans.com>
2022-02-16 10:16:41 +0530

    added search widget

--- lib/presentation/main/components/products_section.dart
+++ lib/presentation/main/components/products_section.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_hub_clone/utils/colors.dart';
+
+import '../widgets/search_widget.dart';
 
 class ProductsSection extends StatelessWidget {
   const ProductsSection({Key? key}) : super(key: key);
@@ -8,10 +11,15 @@ class ProductsSection extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(
         horizontal: 16,
-        ),
+      ),
       width: double.infinity,
       height: 600,
-      color: Colors.white,
+      color: ColorConstants.backgroundColor,
+      child: ListView(
+        children: const [
+          SearchWidget(),
+        ],
+      ),
     );
   }
 }
